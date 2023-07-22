0baa070e66f7ef0e0325172208945024cb11003e
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-07-22 00:13:41 -0700

    dfa finished

--- deterministic-finite-automata-simulation/src/main.rs
+++ deterministic-finite-automata-simulation/src/main.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fs;
 
 // State and transition function where from each state an action only leads to one other state.
 // That is, one action cannot lead us to multiple states from the current state.
@@ -5,24 +8,86 @@
 // automata. Automata means a mathematical model to describe a computational process, or machine
 // Finite means .... finite states that the machine/process can be in
 
-
 // Given an input string run it through DFA simulation based on provided DFA definition file
+#[derive(Debug, Deserialize, Serialize)]
 struct Action {
     action: String,
-    next_state: String
+    next_state: String,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
 struct State {
     state: String,
     allowed_terminal: bool,
-    actions: Vec<Action>, 
+    actions: Vec<Action>,
 }
 
-fn simulation(dfa: Vec<State>, input: String) -> Result<bool> {
+fn simulation(dfa: Vec<State>, input: &String) -> Result<bool, String> {
+    let mut current_state = &dfa[0];
+
+    println!("Init State -> {:?}", current_state);
+    for character in input.chars() {
+        // is there a state we can move to via the car as an action
+        println!(
+            "Searching from transitions {:?} for {}",
+            current_state.actions, character
+        );
+        let valid_transition: Vec<&Action> = current_state
+            .actions
+            .iter()
+            .filter(|&action| action.action == character.to_string())
+            .collect();
+
+        if valid_transition.len() > 1 {
+            return Err(format!(
+                "Action {} cannot have multiple transition options in a DFA",
+                character
+            )
+            .to_string());
+        }
+
+        if valid_transition.len() == 0 {
+            return Ok(false);
+        }
+
+        let next_state = &valid_transition[0].next_state;
+        // find the next_state from our dfa
+        current_state = dfa
+            .iter()
+            .filter(|&s| &s.state == next_state)
+            .last()
+            .unwrap();
+
+        println!("Moved to State -> {:?}", current_state);
+    }
+
+    Ok(current_state.allowed_terminal)
 }
 
 fn main() {
- // read dfa from json file
- // read input from cli
- // run through DFA
+    // read dfa from json file
+    // read input from cli
+    // run through DFA
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Please provide a file name, and input string to run DFA against");
+        return;
+    }
+    let file_name = &args[1];
+    let input_string = &args[2];
+
+    // Read the file contents into a String
+    let file_contents = match fs::read_to_string(file_name) {
+        Ok(contents) => contents,
+        Err(err) => {
+            println!("Error reading file: {}", err);
+            return;
+        }
+    };
+
+    let dfa: Vec<State> = serde_json::from_str(&file_contents).unwrap();
+
+    let result = simulation(dfa, input_string);
+
+    println!("{:?}", result);
 }
