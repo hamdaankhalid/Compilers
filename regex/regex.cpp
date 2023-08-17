698dda8af09a4857fd998075c60ad47aa04e6fdd
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-08-16 23:34:43 -0700

    woohoo first part of MYT construction

--- regex/regex.cpp
+++ regex/regex.cpp
@@ -1,63 +1,45 @@
+#include <memory>
+#include<vector>
 #include <iostream>
+#include <unordered_map>
 
-namespace CharacterCategories {
+/*
+ * Primitives:
+ * - EMPTY 
+ * - DIGIT 
+ * - NUMBER 
+ * - 'literal'
+ * 
+ * Concatenation:
+ * - "-"
+ *
+ * Alternation:
+ * - "|"
+ *
+ * Kleene Star:
+ * - "*"
+ *
+ * Grouping & Paranthesis:
+ * - "( )"
+ * */
 
-namespace CharacterEscapes {
-const char escapeChar = '\\';
-const char tab = 't';
-const char carriageReturn = 'r';
-const char newLine = 'n';
-} // namespace CharacterEscapes
+/*
+ * This class is a node, it can be appended to, and it can be pointed to by others
+ * */
+class Nfa {
+private:
 
-namespace CharacterClasses {
+	bool acceptingState;
+	// multiple exits given
+	std::unordered_map<std::string, std::unique_ptr<Nfa> > relations;
 
-const char negation = '^';
-const char wildcard = '.';
-const char anyWord = 'w';
-const char notAnyWord = 'W';
-const char whiteSpace = 's';
-const char nonWhiteSpace = 'S';
-const char decimalDigit = 'd';
-const char nonDecimalDigit = 'D';
-
-template <typename T> class CharacterGroup {
-  std::vector<T> innerGroup;
-  
-  bool isMatch(T candidate) { return true; }
-
-  bool isNotMatch(T candidate) { return false; }
-};
-} // namespace CharacterClasses
-
-namespace Anchors {
-	const char startOfStringMultiLine = '^';
-	const char endOfStringMultiLine = '$';
-	const char startOfStringSingleLine = 'A';
-	const char endOfStringSingleLine = 'Z';
-} // namespace Anchors
-
-namespace Quantifiers {
-	const char zeroOrMore = '*';
-	const char oneOrMore = '+';
-	const char zeroOrOne = '?';
-} // namespace Quantifiers
-}; // namespace CharacterCategories
-
-// Read a string and create a vector of tokens
-
-class RegexEngine {
 public:
-  static RegexEngine from(const std::string &expr) {
-    RegexEngine ex;
-    return ex;
-  }
 
-  bool is_match(const std::string &expr) { return true; }
+	// Used during construction
+	void addNext(std::string, std::unique_ptr<Nfa>) {
+	}
 };
 
 int main() { 
-	const std::string phoneNumExpr = "\d{3}\-\d{3}-\d{4}";
-	const std::string emailExpr = "(\d|\w\@(\d|\w)\.\c\o\m";
-
 	std::cout << "la la lala" << std::endl; 
 }
